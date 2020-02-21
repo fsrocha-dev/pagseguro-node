b529f0619ee8731e5454b05f5cb05456c4290209
fsrocha.dev <contato.frankrocha@gmail.com>
2020-02-21 14:34:00 -0300

    finalizando a configuração dos métodos e simulando pagamento

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const pgSeguro = require("./pagseguro");
 
 const app = express();
 
@@ -7,7 +8,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/payment", async (req, res) => {
-  return res.send("OK");
+  //Dados do comprador
+  pgSeguro.sender({
+    name: "Frank Souza Rocha",
+    email: "contato@frankrocha.net",
+    phone: {
+      areaCode: "75",
+      number: "992141967"
+    }
+  });
+
+  //Os items da venda, podem ter vários caso haja nescessidade
+  pgSeguro.addItem({
+    id: "10",
+    description: "Payment Test",
+    amount: "20.00",
+    quantity: 1
+  });
+
+  //Informações de Frete e pagamento
+  pgSeguro.shipping({
+    type: 3,
+    name: req.body.name,
+    card: req.body.card,
+    cvv: req.body.cvv,
+    date: req.body.date,
+    address: {
+      street: req.body.address.street,
+      number: req.body.address.number,
+      city: req.body.address.city,
+      state: req.body.address.state,
+      country: req.body.address.country
+    }
+  });
+
+  //Alternativa seria da seguinte forma
+  //Type 3 quer dizer que não tem frete o tal produto.
+  pgSeguro.shipping({
+    type: 3
+  });
+
+  pgSeguro.checkout((data, response) => {
+    if (data) {
+      return res.json({ data });
+    }
+  });
 });
 
 app.listen(3000, () =>
--- pagseguro.js
+++ pagseguro.js
@@ -1,12 +1,21 @@
 const Pagseguro = require("pagseguro-nodejs");
 
+// Seta as variáveis do modo e do token do pagseguro de acordo o ambiente.
+const mode =
+  process.env.NODE_ENV === "development"
+    ? Pagseguro.MODE_SANDBOX
+    : Pagseguro.MODE_PAYMENT;
+
+const tokenDev = "";
+const tokenProd = "";
+
+const token = process.env.NODE_ENV === "development" ? tokenDev : tokenProd;
+
+// Cria a instância do pagseguro.
 const pag = new Pagseguro({
-  email: "",
-  token: "",
-  mode:
-    process.env.NODE_ENV === "development"
-      ? Pagseguro.MODE_SANDBOX
-      : Pagseguro.MODE_PAYMENT
+  email: "contato.frankrocha@gmail.com",
+  token,
+  mode
 });
 
 pag.currency("BRL");
